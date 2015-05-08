712c4b1e2e3b7f581e54ef0e566786ab5488672e
Mathieu Mayer-Mazzoli <mathieu.mayermazzoli@gmail.com>
2015-05-08 09:38:40 +0900

    tests

--- client/main.js
+++ client/main.js
@@ -1,1 +1,24 @@
+Template.post.helpers({
+  'post': function() {
 
+    Meteor.subscribe('posts', this._id);
+
+    return Posts.findOne()
+  }
+});
+
+Template.header.events({
+  'click .app-share-btn': function() {
+
+    var post = {
+      mardown: Session.get('editor-markdown'),
+      html: Session.get('editor-html'),
+      wordCount: Session.get('editor-word-count')
+    }
+
+    post._id = Posts.insert(post);
+
+    Router.go('post', post);
+
+  }
+});
